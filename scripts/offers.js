b1da86f37dd1a38dc4387be7d5cff31b7e6db004
Laura Carvajal <laura.carvajal@gmail.com>
2016-06-08 15:57:28 +0100

    wip

--- scripts/offers.js
+++ scripts/offers.js
@@ -5,27 +5,82 @@
 'use strict';
 const API = require("../lib/apis/offersapi");
 
+const SYMBOL = {
+	gbp: '£',
+	usd: '$',
+	jpy: '¥',
+	eur: '€',
+	aud: 'A$',
+	sgd: 'S$',
+	hkd: 'HK$'
+};
+
+const COUNTRIES = {
+	gbr: 'the UK',
+	usa: 'the US'
+};
+
 module.exports = function (robot) {
 
-	robot.respond(/offers(?:\s+(.*))?/i, function (res) {
+	robot.respond(/(?:(.*)\s)?offers(?:\s+(.*))?/i, function (res) {
 
 		console.log('offers query', API);
 
-		const type = res.match[1];
+		const frequency = res.match[1];
+		const countryCode = res.match[2];
+		const seenValues = {};
 
-		if (!type) {
-			res.send('You need to specify a type, e.g. offers trial');
+		if (!countryCode && !frequency) {
+			res.send('You need to specify a country code, e.g. Monthly offers GBR or All offers USA');
 			return;
 		} else {
 
 			res = require('../lib/autolog')(res); // Log the query to the database
 			// res = require('../lib/progressfeedback')(res);	// Pricing can take a while, so mix in progress feedback behaviour
-			return API.getOffers()
-				.then(function(price) {
-					res.send(price);
+			return API.getOffers(countryCode.toUpperCase(), frequency)
+				.then(function(data) {
+
+					let offers = data.offers;
+
+					console.log('\n\n\noffers', offers);
+					let result;
+
+					for (const keyOffer in offers) {
+						if (offers.hasOwnProperty(keyOffer)) {
+							let charges = offers[keyOffer].charges;
+
+							for (const keyCharge in charges) {
+								if (charges.hasOwnProperty(keyCharge)) {
+
+									const frequency = charges[keyCharge].billingFrequency.displayName;
+									const country = COUNTRIES[offers[0].country.toLowerCase()];
+									const symbol = SYMBOL[charges[keyCharge].amount.symbol.toLowerCase()] || charges[keyCharge.toLowerCase()].amount.symbol;
+									const value = charges[keyCharge].amount.value;
+
+									if (data.frequency.toLowerCase() !== 'all' && data.frequency.toLowerCase() !== frequency.toLowerCase()) {
+										continue;
+									}
+
+									console.log(charges[keyCharge]);
+
+									let thisCharge;
+
+									if (!seenValues[value]) {
+										thisCharge = `*${frequency}* price in ${country} is *${symbol}${value}*`;
+										seenValues[value] = true;
+										result = result ? `${result}\n${thisCharge}` : thisCharge;
+									}
+								}
+							}
+						}
+					}
+
+					const response = result;;
+
+					res.send(response);
 				})
 				.catch(function(e) {
-					console.log(e);
+					// console.lo(e);
 					res.send('Unable to display offers');
 				});
 		}
