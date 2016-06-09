21be18458849d0433e0056dac477783dfe52e876
Laura Carvajal <laura.carvajal@gmail.com>
2016-06-09 16:23:49 +0100

    test code removed

--- scripts/recommend.js
+++ scripts/recommend.js
@@ -16,7 +16,7 @@ module.exports = function (robot) {
 		const term = res.match[1];
 
 		if (! term) {
-			res.send('You need to specify a company name, e.g. recommend apple (I\'m local code)');
+			res.send('You need to specify a company name, e.g. recommend apple');
 			return;
 		} else {
 			API.pricing.findSymbol(term)
